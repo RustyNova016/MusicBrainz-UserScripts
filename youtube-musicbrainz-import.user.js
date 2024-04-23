96ebfbd21116b9bd8ba9a76b4234f07307171ac0
RustyNova016 <50844553+RustyNova016@users.noreply.github.com>
2024-04-23 11:34:27 +0000

    Fixes on youtube-musicbrainz-import.user.js
    Added Userscript links
    Added check whether the link is already in listenbrainz

--- youtube-musicbrainz-import.user.js
+++ youtube-musicbrainz-import.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        MusicBrainz: Import videos from YouTube as release
-// @version     2014-09-07
+// @version     2024-04-23
 // @author      Freso - RustyNova
 // @namespace   df069240-fe79-11dc-95ff-0800200c9a66
 // @require https://raw.github.com/murdos/musicbrainz-userscripts/master/lib/mbimport.js
@@ -10,58 +10,43 @@
 // @homepageURL https://github.com/RustyNova016/MusicBrainz-UserScripts/
 // @supportURL  https://github.com/RustyNova016/MusicBrainz-UserScripts/issues
 // @include     *://www.youtube.com/watch?*
+// @run-at      document-end
+// @grant       GM.xmlHttpRequest
 //
 // Forked from https://bitbucket.org/Freso/nikki-userscripts/src/2bafb61929ed2a4296029e7311bad8f357f44245/youtube-importer/youtube-importer.user.js?fileviewer=file-view-default
 //
 // ==/UserScript==
 //**************************************************************************//
 
+
 var google_api_key = "AIzaSyC5syukuFyCSoRvMr42Geu_d_1c_cRYouU"
+var myform;
+var mysubmit;
+var div;
 
-var myform = document.createElement("form");
-myform.method = "post";
-myform.action = "//musicbrainz.org/release/add";
-myform.acceptCharset = "UTF-8";
-myform.target = "_blank";
 
-mysubmit = document.createElement("input");
-mysubmit.type = "submit";
-mysubmit.value = "Add to MusicBrainz";
-mysubmit.setAttribute("class", "search-button");
-myform.appendChild(mysubmit);
-myform.setAttribute("class", "holder");
-
-var div = document.createElement("div");
-div.setAttribute("class", "holder");
-//div.style.position = 'absolute';
-//div.style.top = 0;
-//div.style.right = 0;
-//div.style.padding = '20px';
-//div.style.margin = '50px';
-//div.style.zIndex = '10000';
-
-var m = document.location.href.match(/\?v=([A-Za-z0-9_-]{11})/);
-if (m && m[1]) {
-	var yt_ws_url = "//www.googleapis.com/youtube/v3/videos?part=snippet,id,contentDetails&id=" + m[1] + "&key=" + google_api_key;
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open('GET', yt_ws_url, true);
-	xmlhttp.onreadystatechange = function() { yt_callback(xmlhttp); }
-	xmlhttp.send(null);
-}
+//var m = document.location.href.match(/\?v=([A-Za-z0-9_-]{11})/);
+//if (m && m[1]) {
+//	var yt_ws_url = "//www.googleapis.com/youtube/v3/videos?part=snippet,id,contentDetails&id=" + m[1] + "&key=" + google_api_key;
+//	var xmlhttp = new XMLHttpRequest();
+//	xmlhttp.open('GET', yt_ws_url, true);
+//	xmlhttp.onreadystatechange = function() { yt_callback(xmlhttp); }
+//	xmlhttp.send(null);
+//}
 
 function yt_callback(req) {
-	if (req.readyState != 4)
-		return;
-	var r = eval('(' + req.responseText + ')').items[0];
+    if (req.readyState != 4)
+        return;
+    var r = eval('(' + req.responseText + ')').items[0];
 
-	var video_id = r.id;
-	var title = r.snippet.title;
-	var artist = r.snippet.channelTitle;
+    var video_id = r.id;
+    var title = r.snippet.title;
+    var artist = r.snippet.channelTitle;
     var publishedAt = new Date(r.snippet.publishedAt);
     var day = publishedAt.getDate()
     var month = publishedAt.getMonth() + 1
     var year = publishedAt.getFullYear()
-	var length = MBImport.ISO8601toMilliSeconds(r.contentDetails.duration);
+    var length = MBImport.ISO8601toMilliSeconds(r.contentDetails.duration);
 
     // New
     add_field("artist_credit.names.0.name", artist);
@@ -79,39 +64,38 @@ function yt_callback(req) {
     add_field("type", "single");
     add_field("urls.0.url", document.location.href);
     add_field("urls.0.link_type", "85");
-    add_field("edit_note", document.location.href + "\n--- \nImported with https://github.com/RustyNova016/MusicBrainz-UserScripts/raw/main/youtube-musicbrainz-import.user.js");
+    add_field("edit_note", document.location.href + "\n --- \nImported with https://github.com/RustyNova016/MusicBrainz-UserScripts/raw/main/youtube-musicbrainz-import.user.js");
 
-	var mb_ws_url = "https://musicbrainz.org/ws/2/url?limit=1&fmt=json&inc=artist-rels+label-rels+release-rels&resource=http://www.youtube.com/watch%3Fv=" + video_id;
-	var xmlhttp2 = new XMLHttpRequest();
-	xmlhttp2.open('GET', mb_ws_url, true);
-	xmlhttp2.onreadystatechange = function() { mb_callback(xmlhttp2); }
-	xmlhttp2.send(null);
+    var mb_ws_url = "https://musicbrainz.org/ws/2/url?limit=1&fmt=json&inc=artist-rels+label-rels+release-rels&resource=" + encodeURI("https://www.youtube.com/watch?v=" + video_id);
+    var xmlhttp2 = new XMLHttpRequest();
+    xmlhttp2.open('GET', mb_ws_url, true);
+    xmlhttp2.onreadystatechange = function() { mb_callback(xmlhttp2); }
+    xmlhttp2.send(null);
 }
 
 function mb_callback(req) {
-	if (req.readyState != 4)
-		return;
-	var r = eval('(' + req.responseText + ')');
+    if (req.readyState != 4){
+        return;
+    }
+    var r = eval('(' + req.responseText + ')');
 
-	if (r.relations) {
-        mysubmit.style.backgroundColor ='#646464';
-		div.innerHTML = "<a href='//musicbrainz.org/url/" + r.id + "'>Already in MB</a> :D";
-		document.body.appendChild(div);
-	} else {
+    if (r.relations) {
+        div.innerHTML = "<div class='holder'><a style='text-decoration: none' href='//musicbrainz.org/url/" + r.id + "'><button style='background-color:#a4a4a4;' class='search-button'>Added ✓</button></a></div>";
+    } else {
         mysubmit.style.backgroundColor = '#BA478F';
-		div.appendChild(myform);
-	}
+        div.appendChild(myform);
+    }
 
     let dock = waitForElementById(`top-level-buttons-computed`).then((dock) => {dock.appendChild(div);})
 
-}
+    }
 
 function add_field (name, value) {
-	var field = document.createElement("input");
-	field.type = "hidden";
-	field.name = name;
-	field.value = value;
-	myform.appendChild(field);
+    var field = document.createElement("input");
+    field.type = "hidden";
+    field.name = name;
+    field.value = value;
+    myform.appendChild(field);
 }
 
 // Insert CSS into the Head
@@ -166,6 +150,29 @@ function main() {
 }
 main();
 
+let globalPromises = []
+let form, entity, formString;
+let previousUrl = '';
+
+new MutationObserver(function(mutations) {
+    if (location.href != previousUrl) {
+        previousUrl = location.href;
+        Promise.all(globalPromises).catch(error => {
+        }).finally(() => {
+            globalPromises = [];
+            onUrlChange();
+        });
+    }
+}).observe(document, {subtree: true, childList: true});
+
+
+// Do all the logic of a page change
+async function onUrlChange() {
+    console.log("Url changed!")
+    reset_state();
+    inject_button();
+}
+
 // Waits until an element appear on the page. This may never happen, so this may hang!
 function waitForElementById(id) {
     return new Promise((resolve, reject) => {
@@ -180,3 +187,48 @@ function waitForElementById(id) {
     });
 }
 
+
+
+function on_mutation() {
+    //reset_state();
+    inject_button();
+    Promise.all(globalPromises)
+}
+
+function reset_state() {
+    myform = document.createElement("form");
+    myform.method = "post";
+    myform.action = "//musicbrainz.org/release/add";
+    myform.acceptCharset = "UTF-8";
+    myform.target = "_blank";
+
+    mysubmit = document.createElement("button");
+    mysubmit.type = "submit";
+    mysubmit.value = "Add to MusicBrainz";
+    mysubmit.setAttribute("class", "search-button");
+    myform.appendChild(mysubmit);
+    myform.setAttribute("class", "holder");
+
+    div = document.createElement("div");
+    div.setAttribute("class", "holder");
+    //div.style.position = 'absolute';
+    //div.style.top = 0;
+    //div.style.right = 0;
+    //div.style.padding = '20px';
+    //div.style.margin = '50px';
+    //div.style.zIndex = '10000';
+}
+
+function inject_button() {
+    console.log("Checking location...")
+    // Check if the url match one of a video
+    var m = document.location.href.match(/\?v=([A-Za-z0-9_-]{11})/);
+    if (m && m[1]) {
+        console.log("Injecting Button")
+        var yt_ws_url = "//www.googleapis.com/youtube/v3/videos?part=snippet,id,contentDetails&id=" + m[1] + "&key=" + google_api_key;
+        var xmlhttp = new XMLHttpRequest();
+        xmlhttp.open('GET', yt_ws_url, true);
+        xmlhttp.onreadystatechange = function() { yt_callback(xmlhttp); }
+        xmlhttp.send(null);
+    }
+}
